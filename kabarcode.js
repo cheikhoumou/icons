cbd7ab28f0eb714ca4b5868c29a7f2f9d5f78943
Cheikh Oumou <67443473+cheikhoumou@users.noreply.github.com>
2021-12-25 16:26:15 +0200

    v2
    v2

--- kabarcode.js
+++ kabarcode.js
@@ -1,1 +1,9 @@
 var codeactive = '0556F7P768858';
+var itemscode = document.querySelector('.header-top #menu-top .HTML .code')
+if( itemscode.innerText == '\n0556F7P768858\n'){
+    console.log('*** القالب مفعل***');
+}else{
+    document.querySelector('.box-cheikho-king').style = "display: block;";
+    window.cheikhohaking.innerHTML += `.container, .footer-wrapper, .copyright, .wrappercheikho, .social-media-tools {filter: blur(6px);}`;
+    console.log('القالب غير مفعل!!!');
+}
